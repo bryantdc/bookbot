48661b0ff6f9abdec6d8ed7c528a90e4b9399549
bryantdc <bryantdc@gmail.com>
2026-01-20 12:29:39 -0500

    change from if else to .get

--- stats.py
+++ stats.py
@@ -5,10 +5,12 @@ def get_num_char(text):
     char_dict={}
     lower_text = text.lower()
     for char in lower_text:
-        if char not in char_dict:
-            char_dict.update({char: 1})
-        elif char in char_dict:
-            char_dict[char] += 1
+        char_dict[char] = char_dict.get(char, 0) + 1
+ 
+ #       if char not in char_dict:
+ #           char_dict.update({char: 1})
+ #       elif char in char_dict:
+ #           char_dict[char] += 1
     return char_dict
 
 def get_num_words(text):
